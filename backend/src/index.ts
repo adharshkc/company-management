d6804368998ec22faf6ecf5f0d23ab54f876352f
adharshkc <adharshkc2017@gmail.com>
2024-07-13 21:31:29 +0530

    added some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
-import path from 'path'
-import app from '../src/frameworks/webserver/ExpressServer'
-import "dotenv/config"
+import app from "../src/frameworks/webserver/ExpressServer";
+import "dotenv/config";
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log("server started on port: ", process.env.PORT)
-})
+app.listen(process.env.PORT, () => {
+  console.log("server started on port: ", process.env.PORT);
+});
